c913db1891ed3a8352511e56f56f2cd5f9803b8d
Pablo Estefo <pestefo@gmail.com>
2018-11-16 15:51:46 -0300

    Tests for WCFAlgorithmNoMemory. First results of sanity check

--- con_rec.py
+++ con_rec.py
@@ -357,20 +357,24 @@ class WCFAlgorithmNoMemory(WCFAlgorithm):
         return self.questions_for_user(user_id) - set([question_id_to_ignore])
 
     # R_uu - Relation between two users
-    def r_uu(self, candidate, user_b, target_question):
-        q_in_common = self.questions_in_common([candidate, user_b]) -\
+    def r_uu(self, candidate, asker, target_question):
+        q_in_common = self.questions_in_common([candidate, asker]) -\
             set([target_question])
 
-        q_a = self.questions_for_user_except(candidate, target_question)
-        q_b = self.questions_for_user(user_b)
+        q_c = self.questions_for_user_except(candidate, target_question)
+        q_a = self.questions_for_user(asker)
 
-        a = sum(map(lambda q: self.r_uq(candidate, q, target_question) *
-                    self.r_uq(user_b, q, target_question),
+        a = sum(map(lambda q:
+                    self.r_uq(candidate, q, target_question) *
+                    self.r_uq(asker, q, target_question),
                     q_in_common))
-        b = sqrt(sum(map(lambda q: self.r_uq(candidate, q, target_question)**2,
-                         q_a)) *
-                 sum(map(lambda q: self.r_uq(user_b, q, target_question)**2,
-                         q_b)))
+
+        b = sqrt(sum(map(lambda q:
+                         self.r_uq(candidate, q, target_question)**2,
+                         q_c)) *
+                 sum(map(lambda q:
+                         self.r_uq(asker, q, target_question)**2,
+                         q_a)))
         if a == 0:
             return 0
         return a / b
@@ -379,14 +383,15 @@ class WCFAlgorithmNoMemory(WCFAlgorithm):
     def r_uq(self, user_id, question_id, target_question):
         # In case of the target question,
         if question_id == target_question:
+            # The only participant of it is the asker, whose R_uq = 1
             if self.get_asker_of_question(question_id) == user_id:
                 return 1
-            # if the user is not the asker, it has 0 in R_uq
+            # if the user is not the asker, R_uq = 0
             return 0
 
         return super(WCFAlgorithm, self).r_uq(user_id, question_id)
 
-    # score(c,q) = R_uu(c, asker(q)) 
+    # score(c,q) = R_uu(c, asker(q))
     # Target question has only the asker as a participant
     def score(self, candidate, question):
         asker = self.get_asker_of_question(question)
