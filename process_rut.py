bde0912d0c05229cfc2e25bf3577464e2a220714
Pablo Estefo <pestefo@gmail.com>
2018-11-27 01:18:24 -0300

    finished processing_rut python

--- process_rut.py
+++ process_rut.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import json
 import csv
+from math import sqrt
 
 
 rutdata = 'data/r_ut.csv'
@@ -16,42 +17,77 @@ def normalize(data):
 
     d_norm = list()
     for d in data:
-        value = float(d[2]) / max_r
+        value = sqrt(float(d[2]) / max_r)
+
         d_norm.append([d[0], d[1], value])
 
     return d_norm
+
+
+def get_tag_table():
+    with open('data/ros_tag.csv') as csvfile:
+        tag_table = dict()
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None) #skipping header
+        for row in reader:
+            print(row)
+            tag_table[row[0]] = row[1]
+    return tag_table
 
 
 def main():
     users = set()
     tags = set()
     rut = list()
-    data = dict()
-    data['nodes'] = list()
-    data['links'] = list()
+    tag_names = get_tag_table()
 
     with open(rutdata, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t')
-        next(reader, None)
+        next(reader, None)  # skipping header
         for row in reader:
             users.add(row[0])
             tags.add(row[1])
             rut.append([row[0], row[1], row[2]])
     rut_normalizado = normalize(rut)
+    data = dict()
 
-    threshold = 0.5
+    # Links
+    data['nodes'] = list()
+    data['links'] = list()
+
+    threshold = 0.45
+    filtered_rut = list()
+
     for row in rut_normalizado:
         if row[2] >= threshold:
-            data['links'].append(
-                {'source': row[0], 'target': row[1], 'value': row[2]})
+            print(str(row[2]) + " -- YES")
+            filtered_rut.append(row)
+        else:
+            print(str(row[2]) + " -- NO")
 
-        for user in users:
-            data['nodes'].append({'id': user, 'group': 1})
+    filtered_users = set()
+    filtered_tags = set()
 
-        for tag in tags:
-            data['nodes'].append({'id': tag, 'group': 2})
+    for row in filtered_rut:
+        filtered_users.add(row[0])
+        filtered_tags.add(row[1])
+
+    # Nodes
+    for user in filtered_users:
+        data['nodes'].append({'id': 'uid:' + str(user), 'group': 1})
+
+    for tag in filtered_tags:
+        data['nodes'].append({'id': tag_names[tag], 'group': 2})
+
+    for row in filtered_rut:
+        data['links'].append(
+            {'source': 'uid:' + str(row[0]),
+             'target': tag_names[row[1]],
+             'value': row[2]})
+
     # Read results, get coverage and print it
-    with open(path_to_results + 'rut_procesado_50percent.json', 'w') as outfile:
+    with open(path_to_results + 'rut_procesado_50percent.json', 'w'
+              ) as outfile:
         json.dump(data, outfile)
 
 
