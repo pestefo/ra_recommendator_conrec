1f95cb2e4760844cab3e924759b5e91fb5c95879
Pablo Estefo <pestefo@gmail.com>
2019-04-03 16:41:06 -0300

    Extracting tags from title and body of questions

--- src/utils/add_question_tags_from_body_and_title.py
+++ src/utils/add_question_tags_from_body_and_title.py
@@ -2,12 +2,22 @@
 # coding: utf-8
 import sqlite3
 from sqlite3 import Error
-from collections import defaultdict
+from collections import defaultdict, Counter
 import re
+import json
 
 conn = None
 database = "data/v1.2.db"
+output_file = 'data/extended_tags.json'
 tag_pattern = None
+stopwords = list()
+
+# Stopwords from https://gist.github.com/sebleier/554280
+with open('src/utils/stopwords.txt', 'r') as fp:
+    for line in fp:
+        stopwords.append(line.rstrip())
+
+
 """
 Adding question tags from the body and the title of a question
 We extract the text from a question and we create a json file
@@ -51,7 +61,9 @@ def execute_query(query):
 def get_data(question_id):
     query = "select title,summary from ros_question where id={}".format(
         question_id)
-    return execute_query(query)[0]
+    title, body = execute_query(query)[0]
+    body = cleanhtml(body)
+    return title, body
 
 
 def get_tags():
@@ -63,19 +75,29 @@ def get_question_ids():
     return [9084, 9097, 9107, 9130, 9135, 9159, 9163, 9166, 9208, 9210, 9259, 9261, 9273, 9287, 9343, 9471, 9511, 9512, 9545, 9553, 9588, 9637, 9640, 9686, 9693, 9766, 9768, 9807, 9815, 9902, 9922, 9942, 9943, 9977, 10026, 10027, 10038, 10047, 10110, 10120, 10130, 10143, 10145, 10166, 10187, 10222, 10245, 10329, 10332, 10335, 10338, 10343, 10356]
 
 
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
 def extract_tags(str):
-    global tag_pattern
+    global tag_pattern, stopwords
 
     matches = re.findall(tag_pattern, str)
 
-    for m in matches:
-        print(m)
+    matches = Counter(matches)
+
+    # removing stopwords
+    for tag in list(matches):
+        if tag in stopwords:
+            del matches[tag]
 
     return matches
 
 
 def main():
-    global tag_pattern
+    global tag_pattern, output_file
     # Question's Title and Body data
 
     # create a database connection
@@ -83,27 +105,27 @@ def main():
 
     questions = get_question_ids()
     tags = get_tags()
-    extended_tags = defaultdict(set)
+    extended_tags = defaultdict(list)
 
-    tag_pattern = '(' + '|'.join(list(map(lambda x: re.escape(x), tags))) + ')'
+    tag_pattern = '\\b(' + \
+        '|'.join(list(map(lambda x: re.escape(x), tags))) + ')\\b'
 
     for q_id in questions[13:]:
 
         print("Question {}".format(q_id))
 
         title, body = get_data(q_id)
-        print(title)
-        extract_tags(title.lower())
 
-        break
+        tags_found = extract_tags(title.lower())
+        tags_found += extract_tags(body.lower())
+        sorted(tags_found)
 
-        extended_tags[q_id].append(extract_tags(title))
+        print(tags_found.keys())
+        extended_tags[q_id].extend(tags_found.keys())
         # extended_tags[q_id].append(extract_tags(body))
-        break
 
-        bag_of_words = get_bag_of_words(body, title)
-
-        register_extended_tags(q_id, bag_of_words)
+    with open(output_file, 'w') as outfile:
+        json.dump(extended_tags, outfile)
 
 
 if __name__ == '__main__':
@@ -111,7 +133,7 @@ if __name__ == '__main__':
 
 '''
 1. Faltan stopwords
-2. kewas los lowerkase ermano
+2. kewas los lowerkase ermanz
 
 Question 9287
 Are opencv samples in diamondback debs?
