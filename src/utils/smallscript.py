e24fdd22450d5b821b9fa037570a557f0bd0231c
Pablo Estefo <pestefo@gmail.com>
2019-07-02 18:59:51 -0400

    Fixed the problem with paths to other python files and data files. Created test for question -> tag mapping. It passes. Created test for question -> extended tag mapping. It fails.

--- src/utils/smallscript.py
+++
@@ -1,7 +0,0 @@
-
-fileName = "/home/pestefo/projects/ra_recommendator_conrec/data/extracted_user_tags_non_extended_2.csv"
-lineList = [line.rstrip('\n') for line in open(fileName)]
-lineList = list(set(lineList))
-
-for line in lineList:
-	print(line)
