e45ada0d7e6c38a3d8cfa7b50cb57a7c0fc610f0
Pablo Estefo <pestefo@gmail.com>
2019-07-10 13:10:10 -0400

    Removed unused methods (printing header and footer).

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -31,38 +31,20 @@ Utils
 """
 
 
-def print_header(scenario):
-    global spec
-    print ("------- start (Scenario {} - {} participants ) -------".format (scenario.name(), spec['NB_OF_PARTICIPANTS']))
-    now = datetime.datetime.now ()
-    print (now.strftime ('%d-%m-%Y %H:%M:%S'))
-    return now
-
-
-def print_footer(start_time, now):
-    print ("\n" + str ((datetime.datetime.now () - now).seconds) + " seconds")
-    print (time.strftime ('%H:%M:%S',
-                          time.gmtime ((
-                                               datetime.datetime.now () - start_time)
-                                       .seconds)) +
-           " elapsed.")
-    print ("------- end -------\n\n")
-
-
 def run_experiment(question_id, filename):
     global spec
-    with open (filename, 'w') as write_file:
-        results = spec['ALGORITHM'].ranking_for_question (question_id,
-                                                          spec['NB_OF_RESULTS'])
-        json.dump (results, write_file)
+    with open(filename, 'w') as write_file:
+        results = spec['ALGORITHM'].ranking_for_question(question_id,
+                                                         spec['NB_OF_RESULTS'])
+        json.dump(results, write_file)
 
 
 def get_sample(nb_of_participants, limit_size=None, dummy_sample=None):
     if dummy_sample:
-        return sample_100q_5p ()
+        return sample_100q_5p()
 
-    db = Database ()  # we don't really make use of scenarios-specific methods
-    sample = db.questions_with_n_participants (nb_of_participants)
+    db = Database()  # we don't really make use of scenarios-specific methods
+    sample = db.questions_with_n_participants(nb_of_participants)
 
     if limit_size:
         return sample[:limit_size]
@@ -74,7 +56,7 @@ def sample_100q_5p():
     # Special sample that i've been using for testing the algorithms
     # There are 100 questions with 5 participants (1 asker + 4 answerers
     # or commenters
-    return files.get_data (files.questions_sample_100q_5p)[:100]
+    return files.get_data(files.questions_sample_100q_5p)[:100]
 
 
 """
@@ -88,28 +70,21 @@ def main():
     global spec
 
     # Get sample of questions
-    spec['SAMPLE'] = get_sample (spec['NB_OF_PARTICIPANTS'], dummy_sample=True)  # 100q_5p sample
+    spec['SAMPLE'] = get_sample(spec['NB_OF_PARTICIPANTS'], dummy_sample=True)  # 100q_5p sample
     # spec['SAMPLE'] = get_sample (spec['NB_OF_PARTICIPANTS'])
 
-    # Start clicking
-    start_time = datetime.datetime.now ()
+    for scenario in Scenario.all_scenarios():
+        spec['ALGORITHM'] = TMBAlgorithm(scenario)
 
-    # for scenario in SCENARIOS:
-    for scenario in spec['SCENARIOS']:
-        spec['ALGORITHM'] = TMBAlgorithm (scenario)
-        # spec['ALGORITHM'] = MockRanking ()
-        now = print_header (scenario)
+        print("\n\n--- Starting {}---\n".format(scenario.name()))
         # Run experiment for each question in the sample
-        for question_id in tqdm(spec['SAMPLE'][: spec['SAMPLE_SIZE']]):
-            # now = print_header (question_id, scenario)
+        for question_id in tqdm(spec['SAMPLE'][: spec['SAMPLE_SIZE']], desc="question:"):
 
-            filename = files.results_file (question_id, scenario, spec['NB_OF_PARTICIPANTS'])
+            filename = files.results_file(question_id, scenario, spec['NB_OF_PARTICIPANTS'])
 
             # Process and write results
-            run_experiment (question_id, filename)
-
-            # print_footer (start_time, now)
+            run_experiment(question_id, filename)
 
 
 if __name__ == '__main__':
-    main ()
+    main()
