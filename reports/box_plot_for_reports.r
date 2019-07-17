ab9ea394a0c0d233fd0f5fe7ba43ad7b5deaaa55
Pablo Estefo <pestefo@gmail.com>
2019-07-17 14:28:20 -0400

    Results for p=2,3,4,5,6,7,8,9,10. Notebook for reporting results

--- reports/box_plot_for_reports.r
+++ reports/box_plot_for_reports.r
@@ -2,13 +2,15 @@ library(ggplot2)
 library(readr)
 require(reshape2)
 
+prefix = "~/projects/ra_recommendator_conrec/results/20190710_1914_5p/recall_for_scenario_"
+scenarios = c("A.csv", "B.csv", "C.csv", "D.csv")
 
 #data <- read.delim("~/projects/experiment_1/data/wcfa_100q_5p_2nd_exp/results_wcfa_100q_5p_2nd_exp.csv")
 #data <- read.delim("~/projects/experiment_1/data/wcfa_100q_5p/results_wcfa_100q_5p.csv")
 #data <- read.delim("~/projects/experiment_1/data/tmba_100q_5p/results_tbma_100q_5p.csv")
 #data <- read.delim("~/projects/experiment_1/data/tmba_100q_1p/results_tmba_100q_1p.csv")
 
-
+data <- read.delim(paste(prefix,scenarios[1]))
 
 
 ggplot(data = melt(data[1:length(data)],id='q_id'), aes(x=variable, y=value)) + 
