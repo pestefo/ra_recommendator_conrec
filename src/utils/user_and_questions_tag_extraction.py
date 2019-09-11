21adedc1377a3f2630d200f722dc1e79debc54ec
Pablo Estefo <pestefo@gmail.com>
2019-09-11 00:25:31 -0300

    dsa

--- src/utils/user_and_questions_tag_extraction.py
+++ src/utils/user_and_questions_tag_extraction.py
@@ -17,48 +17,52 @@ tags from the questions in which the user has previously participated), eg.
 
 
 def extract_extended_tags_for_users():
-    db = Database ()
-    tag_extractor = QuestionTagExtractor ()
+    db = Database()
+    tag_extractor = QuestionTagExtractor()
     user_tags = {}
 
-    users = db.all_users ()
-    shuffle (users)
+    users = db.all_users()
+    shuffle(users)
     print ("Extracting tags...\n")
-    for user in tqdm (users):
-        user_tags[user] = set ()
-        questions = db.questions_where_user_participated (user)
+    for user in tqdm(users):
+        user_tags[user] = set()
+        questions = db.questions_where_user_participated(user)
         for question in questions:
             try:
-                user_tags[user] = user_tags[user].union (tag_extractor.full_extended_tag_ids_for (question))
+                user_tags[user] = user_tags[user].union(
+                    tag_extractor.full_extended_tag_ids_for(question))
             except sqlite3.OperationalError:
-                print ("Operantional ERROR on question: {} \t user: {}".format (question, user))
+                print ("Operantional ERROR on question: {} \t user: {}".format(
+                    question, user))
 
-        user_tags[user] = list (user_tags[user])
+        user_tags[user] = list(user_tags[user])
 
     print ("DONE\n")
-    with open (files.user_full_extended_tags, 'w') as fp:
-        json.dump (user_tags, fp)
+    with open(files.user_full_extended_tags, 'w') as fp:
+        json.dump(user_tags, fp)
 
 
 def extract_extended_tags_for_questions():
-    db = Database ()
-    tag_extractor = QuestionTagExtractor ()
+    db = Database()
+    tag_extractor = QuestionTagExtractor()
     question_tags = {}
 
-    questions = db.all_questions ()
+    questions = db.all_questions()
 
     print ("Extracting tags...\n")
-    for question in tqdm (questions):
+    for question in tqdm(questions):
         question_tags[question] = []
         try:
-            question_tags[question] = list (tag_extractor.full_extended_tag_ids_for (question))
+            question_tags[question] = list(
+                tag_extractor.full_extended_tag_ids_for(question))
         except sqlite3.OperationalError:
-            print ("Operantional ERROR on question: {} ".format (question))
+            print ("Operantional ERROR on question: {} ".format(question))
 
     print ("DONE\n")
 
-    with open (files.question_full_extended_tags, 'w') as fp:
-        json.dump (question_tags, fp)
+    with open(files.question_full_extended_tags, 'w') as fp:
+        json.dump(question_tags, fp)
+
 
 def invert_dict(d):
     inverse = dict()
@@ -73,20 +77,19 @@ def invert_dict(d):
                 inverse[item].append(key)
     return inverse
 
+
 def reversing_the_extended_tags_for_questions_dictionary():
     with open(files.question_full_extended_tags, 'r') as fp:
         d = json.load(fp)
     tags_to_questions = invert_dict(d)
 
-    with open (files.full_extended_tags_to_questions, 'w') as fp:
-        json.dump (tags_to_questions, fp)
+    with open(files.full_extended_tags_to_questions, 'w') as fp:
+        json.dump(tags_to_questions, fp)
 
 
 def main():
     pass
 
 
-
-
 if __name__ == '__main__':
-    main ()
+    main()
